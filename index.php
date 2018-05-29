a43ec744245da6f1cdad530f2ab9feb46afd91aa
Amir Al Hafiz <34679121+amiralhafiz@users.noreply.github.com>
2018-05-29 21:33:23 +0800

    Update index.php
    update navbar javascript

--- index.php
+++ index.php
@@ -26,8 +26,10 @@
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
       <!-- Include all compiled plugins (below), or include individual files as needed -->
-      <script src="/assets/js/bootstrap.min.js"></script>
-      <script src="/assets/js/sticky.js"></script>
+	   <script src="assets/js/slim.min.js"></script> 
+      <script src="assets/js/popper.min.js"></script> 
+      <script src="assets/js/bootstrap.min.js"></script> 
+      <script src="assets/js/sticky.js"></script>
       <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
       <!-- WARNING: Respond.js doesn't work if you view the pphonenumber via file:// -->
       <!--[if lt IE 9]>
